7f9e3d89d5e434146c87e828421db57110b2cce5
vidhya199 <vidhyadharlambade@gmail.com>
2021-05-18 22:54:15 +0530

    passing argument to the script at run time

---
+++ t2_passing_arguments/pr1_passing_command_line_args.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# programm for test passing argument in to the file using command line.
+
+echo "Argument 0: " $0
+echo "Argument 1: " $1
+echo "Argument 2: " $2
+echo "Argument 3: " $3
+
+# printing array
+args=("$@")
+echo "Printing the array : "
+echo ${args[0]} ${args[1]} ${args[2]}
